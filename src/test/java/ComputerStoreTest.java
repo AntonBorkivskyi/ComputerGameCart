85b4d03b07b4d92d58be18d83bcf6d7c0dbbcde5
AntonBorkivskyi <anton.borkivskyi@gmail.com>
2017-11-11 20:44:45 +0200

    Renaming2

--- src/test/java/ComputerStoreTest.java
+++ src/test/java/ComputerStoreTest.java
@@ -2,27 +2,27 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class CartTest {
+public class ComputerStoreTest {
     @Test
     public void addGame() throws Exception {
-        Cart cart = new Cart();
-        cart.addGame("N1", 100, 1, new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS));
-        assertEquals(1, cart.getGames().size());
-        cart.addGame("N2", 200, 2, new ComputerGameParameters(12, "D2", Genres.MOBA, Platforms.WINDOWS));
-        assertEquals(2, cart.getGames().size());
+        ComputerStore computer_store = new ComputerStore();
+        computer_store.addGame("N1", 100, 1, new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS));
+        assertEquals(1, computer_store.getGames().size());
+        computer_store.addGame("N2", 200, 2, new ComputerGameParameters(12, "D2", Genres.MOBA, Platforms.WINDOWS));
+        assertEquals(2, computer_store.getGames().size());
 
     }
 
     @Test
     public void search() throws Exception {
-        Cart cart = new Cart();
-        cart.addGame("N1", 100, 1, new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS));
-        cart.addGame("N2", 200, 2, new ComputerGameParameters(12, "D2", Genres.MOBA, Platforms.WINDOWS));
-        cart.addGame("N3", 300, 3, new ComputerGameParameters(15, "D3", Genres.SIMULATION, Platforms.LINUX));
-        cart.addGame("N4", 400, 4, new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS));
-        assertEquals(2, cart.search(new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS)).size());
-        assertEquals(0, cart.search(new ComputerGameParameters(15, "D3", Genres.MOBA, Platforms.WINDOWS)).size());
-        assertEquals(4, cart.getGames().size());
+        ComputerStore computer_store = new ComputerStore();
+        computer_store.addGame("N1", 100, 1, new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS));
+        computer_store.addGame("N2", 200, 2, new ComputerGameParameters(12, "D2", Genres.MOBA, Platforms.WINDOWS));
+        computer_store.addGame("N3", 300, 3, new ComputerGameParameters(15, "D3", Genres.SIMULATION, Platforms.LINUX));
+        computer_store.addGame("N4", 400, 4, new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS));
+        assertEquals(2, computer_store.search(new ComputerGameParameters(18, "D1", Genres.ACTION, Platforms.IOS)).size());
+        assertEquals(0, computer_store.search(new ComputerGameParameters(15, "D3", Genres.MOBA, Platforms.WINDOWS)).size());
+        assertEquals(4, computer_store.getGames().size());
     }
 
     @Test
